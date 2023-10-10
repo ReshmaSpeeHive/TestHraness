8a1b9534252f95e654b113750cea3df9957ac46a
Reshma Raj <reshma.raj@speehive.com>
2023-10-10 23:38:02 +0530

    method, params getting and invoking

--- TestHraness/Program.cs
+++ TestHraness/Program.cs
@@ -7,32 +7,47 @@ List<System.Type> classes= assembly.GetTypes()
     .Where(t=>t.Namespace == TargetNamespace).ToList();
 ThisClass.WritePromptToScreen("Please press the number key associated with " +
                                    "the class you wish to test");
-int count = 0;
-foreach (System.Type t in classes)
-{
-    count++;
-    Console.WriteLine($"{count}.{t}");
-}
-ConsoleKey consoleKey = Console.ReadKey().Key;
-Type classChoice = null;
-switch (consoleKey)
-{
-    case ConsoleKey.D1:
-        classChoice = classes[0];
-        break;
-    case ConsoleKey.D2:
-        classChoice = classes[1];
-        break;
-    case ConsoleKey.D3:
-        classChoice = classes[2];
-        break;
-    case ConsoleKey.D4:
-        classChoice = classes[3];
-        break;
-}
-object classInstance = Activator.CreateInstance(classChoice, null);
+//int count = 0;
+//foreach (System.Type t in classes)
+//{
+//    count++;
+//    Console.WriteLine($"{count}.{t}");
+//}
+ThisClass.DisplayProgramElementList(classes);
+Type typeChoice=ThisClass.ReturnProgramElementReferenceFromList(classes);
+
+//ConsoleKey consoleKey = Console.ReadKey().Key;
+//Type classChoice = null;
+//switch (consoleKey)
+//{
+//    case ConsoleKey.D1:
+//        classChoice = classes[0];
+//        break;
+//    case ConsoleKey.D2:
+//        classChoice = classes[1];
+//        break;
+//    case ConsoleKey.D3:
+//        classChoice = classes[2];
+//        break;
+//    case ConsoleKey.D4:
+//        classChoice = classes[3];
+//        break;
+//}
+object classInstance = Activator.CreateInstance(typeChoice, null);
 Console.Clear();
-ThisClass.WriteHeadingToScreen($"Class: '{classChoice}'");
+ThisClass.WriteHeadingToScreen($"Class: '{typeChoice}'");
+ThisClass.WritePromptToScreen("Please enter the number associated with the method you wish to test");
+List<MethodInfo> methods = typeChoice.GetMethods().ToList();
+ThisClass.DisplayProgramElementList(methods);
+MethodInfo methodChoice = ThisClass.ReturnProgramElementReferenceFromList(methods);
+if (methodChoice != null)
+{
+    Console.Clear();
+
+    ThisClass.WriteHeadingToScreen($"Class: '{typeChoice}' - Method: '{methodChoice.Name}'");
+    ParameterInfo[] parameters = methodChoice.GetParameters();
+
+}
 public static class ThisClass
 {
     public static void WritePromptToScreen(string promptText)
@@ -47,4 +62,34 @@ public static class ThisClass
         Console.WriteLine();
 
     }
+    public static void DisplayProgramElementList<T>(List<T> list)
+    {
+        int count = 0;
+
+        foreach (var item in list)
+        {
+            count++;
+            Console.WriteLine($"{count}. {item}");
+        }
+
+    }
+    public static T ReturnProgramElementReferenceFromList<T>(List<T> list)
+    {
+        ConsoleKey consoleKey = Console.ReadKey().Key;
+
+        switch (consoleKey)
+        {
+            case ConsoleKey.D1:
+                return list[0];
+            case ConsoleKey.D2:
+                return list[1];
+            case ConsoleKey.D3:
+                return list[2];
+            case ConsoleKey.D4:
+                return list[3];
+
+        }
+        return default;
+    }
+
 }
